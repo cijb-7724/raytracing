7ce9bdcd68993a328e666140da65172ff6eecc9e
cijb_7724 <johokamia0@gmail.com>
2024-09-23 22:40:18 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return(
+      <>
+          <h1>hoge</h1>
+          <p className="text-red-600 font-bold text-5xl ">
+            Hello Tailwind
+          </p>
+      </>
+  )
+}
