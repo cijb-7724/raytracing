82afb2bd02b028dd61d4ad678025f49b1816dc4d
cijb_7724 <johokamia0@gmail.com>
2024-10-16 10:54:51 +0900

    feat: 模様選択のモーダルを作成

---
+++ src/components/Modal.tsx
@@ -0,0 +1,79 @@
+import DrawTilePattern from "./DrawTitlePattern";
+
+type ModalProps = {
+  color1: string;
+  color2: string;
+  onClose: () => void;
+  onPatternChange: (pattern: string) => void;
+};
+  
+export default function Modal({ color1, color2, onClose, onPatternChange}: ModalProps) {
+  const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // モーダル外のクリックを検知して閉じる
+    if ((event.target as HTMLDivElement).id === "modal-overlay") {
+      onClose();
+    }
+  };
+  
+  return (
+    <div
+      id="modal-overlay"
+      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={handleOutsideClick}
+    >
+      <div className="bg-white p-6 rounded-lg shadow-lg relative z-60">
+        <button
+          className="absolute top-2 right-2 text-xl bg-black text-white rounded-full p-2"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+
+        <h2 className="text-2xl mb-4">Select a Pattern</h2>
+
+        <div className="grid grid-cols-2 gap-4">
+          <div onClick={() => {
+            onClose();
+            onPatternChange("normal");
+          }}>
+            <DrawTilePattern
+              color1={color1}
+              color2={color2}
+              pattern="nomal"
+            />
+          </div>
+          <div onClick={() => {
+            onClose();
+            onPatternChange("star");
+          }}>
+            <DrawTilePattern
+              color1={color1}
+              color2={color2}
+              pattern="start"
+            />
+          </div>
+          <div onClick={() => {
+            onClose();
+            onPatternChange("apple");
+          }}>
+            <DrawTilePattern
+              color1={color1}
+              color2={color2}
+              pattern="apple"
+            />
+          </div>
+          <div onClick={() => {
+            onClose();
+            onPatternChange("abcd");
+          }}>
+            <DrawTilePattern
+              color1={color1}
+              color2={color2}
+              pattern="abcd"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
