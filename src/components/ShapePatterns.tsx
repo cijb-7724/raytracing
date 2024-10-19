d54ebfe73a9bc623ba65006086519ca6918a7b17
cijb_7724 <johokamia0@gmail.com>
2024-10-20 02:50:01 +0900

    fix: starの角度を修正

--- src/components/ShapePatterns.tsx
+++ src/components/ShapePatterns.tsx
@@ -20,27 +20,29 @@ export const star = (w: number, x: number, y: number): boolean => {
   const r = (w / 2) * 0.8;
   let ok = true;
   const theta = Math.PI * 4 / 5;
+  const base = Math.PI/2;
   const as = Array(5).fill(0); // 長さ5の配列を初期化
   const bs = Array(5).fill(0); // 長さ5の配列を初期化
 
   for (let k = 0; k < 5; ++k) {
-    const xp = r * Math.cos(Math.PI/2 + theta * k);
-    const yp = r * Math.sin(Math.PI/2 + theta * k);
-    const xq = r * Math.cos(Math.PI/2 + theta * (k + 1));
-    const yq = r * Math.sin(Math.PI/2 + theta * (k + 1));
+    const xp = r * Math.cos(base + theta * k);
+    const yp = r * Math.sin(base + theta * k);
+    const xq = r * Math.cos(base + theta * (k + 1));
+    const yq = r * Math.sin(base + theta * (k + 1));
     const a = (yq - yp) / (xq - xp);
     const b = yp - a * xp; // 直線の傾きと切片を計算
 
     as[k] = a;
     bs[k] = b;
   }
+  x *= -1;
 
   // 複数の条件をチェックして"ok"のフラグを設定
-  if (y > as[0] * x + bs[0] && y > as[2] * x + bs[2]) ok = false;
-  if (y < as[3] * x + bs[3] && y > as[0] * x + bs[0]) ok = false;
-  if (y < as[1] * x + bs[1] && y < as[3] * x + bs[3]) ok = false;
-  if (y > as[4] * x + bs[4] && y < as[1] * x + bs[1]) ok = false;
-  if (y > as[2] * x + bs[2] && y > as[4] * x + bs[4]) ok = false;
+  if (x > as[0] * y + bs[0] && x > as[2] * y + bs[2]) ok = false;
+  if (x < as[3] * y + bs[3] && x > as[0] * y + bs[0]) ok = false;
+  if (x < as[1] * y + bs[1] && x < as[3] * y + bs[3]) ok = false;
+  if (x > as[4] * y + bs[4] && x < as[1] * y + bs[1]) ok = false;
+  if (x > as[2] * y + bs[2] && x > as[4] * y + bs[4]) ok = false;
 
   return ok;
 };
